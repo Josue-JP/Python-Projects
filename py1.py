515c16edd435ceca7578919d86e327372d347dc9
Josue-JP <josueportillod07@gmail.com>
2025-01-07 15:34:44 -0500

    Modifying py1.py

--- py1.py
+++ py1.py
@@ -1,93 +1,95 @@
-####################################
-#     This code is free to use     #
-####################################
-class Error(Exception):
-    '''This will display an error'''
+######################################
+#      This code is free to use      # 
+######################################
+# In this code, Q and q means question
+class Invalid_Response(Exception):
+    '''This will throw a custom error'''
     pass
 
-def Check_For_Empty(value):
-    '''This throws an error msg if the input is empty'''
+def Empty_Input_Check(value): 
+    '''This will throw a custom error if any of the values are empty'''
     if not value: 
-        raise Error("  INPUT CANNOT BE EMPTY")
-####################################
-def Question_Number():
+        raise Invalid_Response("EMPTY INPUTS ARE NOT ALLOWED")
+#####################################
+def Number_Q():
     while True:
-        try: 
-            firstQuestion = input('Options available:\nEncrypt[1]\nDecrypt[2]\nChoose a Number: ').strip()
-            if firstQuestion not in ['1', '2']:
-                raise Error("  INVALID CHOICE\n  Enter numbers 1 or 2")
-            return firstQuestion
-        except Error as e: 
-            print(f"  ERROR: {e}")
-###################################
-def Question_Direction(encryption):
+        try:
+            numberQ = input('\nChoose one: Encrypt[1] | Decrypt[2]').strip()
+            if numberQ not in ['1', '2']:
+                raise Invalid_Response("ENTER NUMBERS 1 OR 2")
+            return numberQ
+        except Invalid_Response as e: 
+            print(f'| {e}') 
+
+            
+#####################################
+def Direction_Q(encrypting):
     while True: 
         try: 
-            prompt = 'Enter a positive direction: ' if encryption else 'Enter a negative direction: '
-            direction = int(input(prompt))
+            directionQ = "Enter a positive direction: " if encrypting else "Enter a negative direction: "
+            direction = int(input(directionQ))
 
-            if (encryption and direction <= 0) or (not encryption and direction >= 0):
-                raise Error('  INVALID DIRECTION\n  Direction must be positive for encryption and negative for decryption')
+            if (encrypting and direction <= 0) or (not encrypting and direction >= 0):
+                raise Invalid_Response("INVALID DIRECTION | DIRECTION MUST BE POSITIVE FOR ENCRYPTION AND NEGATIVE FOR DECRYPTION")
             return direction 
-        except ValueError:
-            print('  ERROR: Enter a valid number')
-        except Error as e:
-            print(f"  ERROR: {e}")
-###################################
-def Cipher(msg, key, direction):
+        except ValueError: 
+            print("| Invalid number value")
+        except Invalid_Response as e:
+            print(f"| {e}")
+#####################################
+def Cipher(message, key, direction):
     customAlphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnÑñOoPpQqRrSsTtUuVvWwXxYyZzÁáÉéÍíÓóÚúÜü1234567890,./;'[]=-)(*&^%$#@!<>?:{}+_"
-    finalMsg = ''
+    finalMessage = ''
     keyIndex = 0
 
-    for Character in msg: 
-        if Character not in customAlphabet: 
-            finalMsg += Character # This appends the original character of every character that is not in the customAlphbet
-            continue
-        else:             
-
-            keyCharacter = key[keyIndex % len(key)] # This line gets the key index of the input, and the % len(key) makes sure that there is always a valid charcter selected, due to the index always going up by 1 because of the next line
-            keyIndex += 1 # This makes sure that all characters are selected 
-
-            offset = customAlphabet.index(keyCharacter) # This selects the index of customAlphabet according to the number of keyCharacter
-            
-            msgIndex = customAlphabet.find(Character)
-
-            newMsgIndex = (msgIndex + offset * direction) % len(customAlphabet)
-            
-            finalMsg += customAlphabet[newMsgIndex]
-
-    return finalMsg
-###################################
-def Main(): 
-    try: 
-        enOrDecryptQuestion = Question_Number()
-        encryption = enOrDecryptQuestion == '1'
-        Check_For_Empty(enOrDecryptQuestion)
-
-        keyQuestion = input('Enter your key:').strip()
-        Check_For_Empty(keyQuestion)
-
-        directionQuestion = Question_Direction(encryption)
-        Check_For_Empty(directionQuestion)
-
-        textQuestion = input('Enter your text: ').strip()
-        Check_For_Empty(textQuestion)
-
-
-        Final_Result = Cipher(textQuestion, keyQuestion, directionQuestion)
+    for character in message:
+        if character not in customAlphabet:
+            finalMessage += character # This allows for unknown characters to be inserted into the fianlMessage without tampering it
         
-        operation = 'E' if encryption else "D" # E stands for Encryption and D for Decryption
-        print(f'\n{operation}:\n  {Final_Result}')
+        else:
+            keyCharacter = key[keyIndex % len(key)] # This obtains the current character of the key input
+            # len(key) ensures that there ^^^^^^^ is always a valid character selected 
+            keyIndex += 1 # This ensures that each letter is selected
 
-    except Error as e:
-        print(f'ERROR: {e}')
+            offset = customAlphabet.index(keyCharacter) # This selects the index of keyCharacter in the customAlphabet
+
+            messageIndex = customAlphabet.find(character)
+
+            newMessageIndex = (messageIndex + offset * direction) % len(customAlphabet)
+            # len(key) ensures that there is always a valid character selected 
+
+            finalMessage += customAlphabet[newMessageIndex]
+
+    return finalMessage
+#####################################
+def Main():
+    try:
+        qNumber = Number_Q()
+        encrypting = qNumber == "1"
+
+        qKey = input("Enter a key: ").strip()
+        Empty_Input_Check(qKey) # This will check if qKey is empty
+
+        qDirection = Direction_Q(encrypting)
+
+        qText = input("Enter your text: ").strip()
+        Empty_Input_Check(qText) # This will check if qText is empty
+
+
+        
+        Final_Result = Cipher(qText, qKey, qDirection)
+
+        EorD = "E" if encrypting else "D" # E for Encrypt, D for Decrypt
+        print(f"{EorD}_RESULT:\n  {Final_Result}")
+    except Invalid_Response as e: 
+        print(f"|| {e}")
     except KeyboardInterrupt:
-        print("\nProcess cancelled by user")
-    except Exception as e: 
-        print(f'Error : {e}')
+        print("\nPROCESS ENDED BY USER")
+    except Exception as e:
+        print(f"||| {e}")
 
 if __name__ == "__main__":
-    Main() 
+    Main()
 # This if statement was placed so that if this file is used as a module or something like that, it wouldn't start the code on launch/automatically.
 
 
